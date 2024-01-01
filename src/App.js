5b482472b709843107b2291c3ba680edaa0b7ccd
aliyudhati <aliyudhati12@gmail.com>
2024-01-01 15:56:19 +0700

    codingan hny siap

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import Particles from "react-particles";
+import { loadFireworksPreset } from "tsparticles-preset-fireworks"
+import { Typewriter } from "react-simple-typewriter";
+import { useState } from "react";
+
+function App() {
+  const [newYearMessage, setNewYearMessage] = useState(["Selamat", "Tahun", "Baru", "Syifa Azzahra", "Bye 2023", "Bye Syifa"])
+
+  const particleInitialization = async(engine) => {
+    await loadFireworksPreset(engine)
+  }
+
+  return (
+    <>
+      <Particles 
+        init={particleInitialization}
+        options={{ preset: "fireworks" }}    
+      />
+      <div className="flex flex-col justify-center items-center min-h-screen">
+        <span className="text-white text-4xl font-bold z-50">
+          <Typewriter
+            words={(newYearMessage)}
+            loop={false}
+            cursorStyle={"_"}
+            cursor
+            />
+        </span>
+      </div>
+    </>
+  );
+}
+
+export default App;
